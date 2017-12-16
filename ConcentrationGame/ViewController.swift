99d257a4e39557fdf172951260f8ce0614ab7b91
Tanya Tanyoosha <t.tanyoosha@gmail.com>
2017-12-16 12:20:31 +0300

    Initial Commit

---
+++ ConcentrationGame/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ConcentrationGame
+//
+//  Created by Татьяна Трофимова on 16.12.2017.
+//  Copyright © 2017 Татьяна Трофимова. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
